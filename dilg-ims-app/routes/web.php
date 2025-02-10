f69417f485c657e14b428244fda269df450843cd
SJ Mosquite <sjtherasylvie.mosquite@wvsu.edu.ph>
2025-02-10 16:12:26 +0800

    templates

--- dilg-ims-app/routes/web.php
+++ dilg-ims-app/routes/web.php
@@ -1,20 +1,19 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::view('/', 'admin.admin-dashboard');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::view('dashboard', 'dashboard')
+    ->middleware(['auth', 'verified'])
+    ->name('dashboard');
 
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+Route::view('profile', 'profile')
+    ->middleware(['auth'])
+    ->name('profile');
+
+    Route::prefix('admin')->group(function () {
+        Route::view('admin-dashboard', 'admin.admin-dashboard')->name('admin-dashboard');
+    });
 
 require __DIR__.'/auth.php';
